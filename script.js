e2b61ba0d8e8b8c464c8096f946392c0c1315a5c
Fylo2 <itaypeetsy@gmail.com>
2025-07-15 00:29:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const SUPABASE_URL = "https://gtoqrlmemebbhhamlvsz.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0b3FybG1lbWViYmhoYW1sdn6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MjQ0MDksImV4cCI6MjA2ODEwMDQwOX0.7ksgOB6sa6yzbNh1GqK7detDSxd28zisxu7EKVVS27k";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
